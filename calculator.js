17778346f951b4423c14d350237c8cd95d212df5
Simon Hall <simon970@gmail.com>
2020-03-10 12:57:03 +1300

    prevent number entries with more than one decimal

--- calculator.js
+++ calculator.js
@@ -3,6 +3,7 @@
 //write function for dealing with multiple operators.  Allow plus minus and minus plus but no other double operators.
 //Could be as simple as returning an error after evaluation on incorrect expressions.
 
+
 var expression=[];
 var display=[];
 
@@ -12,43 +13,41 @@ function getValue() {
    switch (event.target.className) {
 
    case "num": 
-      if(expression[expression.length-1]=="=") {
-      expression=[];
-      display=[]
-      }
+      if ((event.target.value == ".") && (display.includes("."))) {
+         return} //restrict entries to single decimal
       display.push(event.target.value);
       setDisplay();
       break;
 
    case "operator":
       if (expression[expression.length-1]=="=") {
-         if(event.target.value == "=") {
+         if(event.target.value == "=") { //nullify "=" button on repetitive clicks
             return;
-         }else{
+         }else{ //allow new operations to be performed on the last result
             expression=[]
             expression.push(display.join(''),event.target.value)
             return resetMain()
          }
       }
-      if(event.target.value == "="){
+      if(event.target.value == "="){ //resolve an equation on the equals
          expression.push(display.join(''));
-         display=[eval(expression.join(' '))];
+         display=[eval(expression.join(' '))]; 
          expression.push(event.target.value)
          setDisplay();
       }else{
-         expression.push(display.join(''),(event.target.value));
+         expression.push(display.join(''),(event.target.value)); 
          resetMain();
       }
       break;
 
    case "clear": 
-      if (expression[expression.length-1]=="=") {
+      if (expression[expression.length-1]=="=") {//if euqation is resolved all clear buttons clear everything
          expression=[];
          display=[];
          setDisplay();
-      }else if (event.target.id == "c"){
+      }else if (event.target.id == "ce"){ //clears entry leaves previous entries in top display
          resetMain()
-      }else {
+      }else {//AC resets everything back to start
          expression=[]
          display=[];
          setDisplay();
@@ -56,11 +55,12 @@ function getValue() {
    }
 }
 
-function resetMain() {
+function resetMain() { //some refactoring to reset the main display
    setDisplay();
    display=[];
 }
 
+//function to set the displays
 function setDisplay () {
    document.getElementById("display-bottom").innerHTML=display.join('');
    document.getElementById("display-top").innerHTML=expression.join(' ')
