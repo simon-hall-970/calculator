0643daa55e45e1aa8cbd272f66071f0b41b2db20
Simon Hall <simon970@gmail.com>
2020-03-09 14:47:34 +1300

    write html structure and css styling

---
+++ calculator.js
@@ -0,0 +1,11 @@
+/* create an array variable to store the entries
+   create a variable to store the total
+   
+   create a function to get the value of the button and action accordingly
+
+   buttons should be 
+   CE (Clear Everything) when this button is pressed it clears everything and resets all temporary storage
+   C (Clear entry) This clears everything since the last operator
+   show a dvision sign but change this value on press to result in /
+   multiplication should show X but when pressed will send a value *
+   
